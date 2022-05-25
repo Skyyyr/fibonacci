ec876ed441735c1988c8a3a0e1579b9bd2e5c39a
Skyyyr <emuswgluvr@gmail.com>
2022-05-25 12:42:54 -0700

    Python fib

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,6 +1,14 @@
 const fibonacci = (num) => {
-    //TEST
-    
+    let n1 = 0, n2 = 1, nextNum;
+    if (num === 0) {
+        return 0;
+    }
+    for (let i = 1; i < num; i++) {
+        nextNum = n1 + n2;
+        n1 = n2;
+        n2 = nextNum;
+    }
+    return nextNum;
 }
 
 module.exports = {fibonacci}
--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,19 @@
-def fibonacci(n):
-  pass
+from termios import NL1
 
+
+def fibonacci(n):
+  # Start with initializing the 2 variables
+  n1 = 0
+  n2 = 1
+  # Set this to "" just to initialize the variable since in Python you cannot initialize without value
+  next = ""
+
+  # Case to close the recursive function
+  if n == 0:
+    return 0
+  for idx in range(1, n):
+    nextNum = n1 + n2
+    n1 = n2
+    n2 = nextNum
+  return nextNum
 
